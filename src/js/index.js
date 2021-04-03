f65d41e21049ec8bda8c18e1050efda8684b120d
vipin singh <svipin174@gmail.com>
2021-04-03 05:39:43 -0700

    plans page added

--- src/js/index.js
+++ src/js/index.js
@@ -148,6 +148,44 @@ const swiper5 = new Swiper('.swiper5', {
     },
 });
 
+const swiper7 = new Swiper('.swiper7', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    slidesPerGroup: 1,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: '.swiper-pagination7',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next7',
+      prevEl: '.swiper-button-prev7',
+    },
+    breakpoints: {
+        576: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+            slidesPerGroup: 1,
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+            slidesPerGroup: 2,
+        },
+        1024: {
+            slidesPerView: 2,
+            spaceBetween: 40,
+            slidesPerGroup: 2,
+        },
+        1280: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+            slidesPerGroup: 3,
+        },
+    }
+  });
+
 // ===================================================================
 //                     ACCORDIAN 
 // ===================================================================
